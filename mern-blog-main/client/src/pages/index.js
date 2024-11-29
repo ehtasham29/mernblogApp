1574e7e7943eb68e11e83342cddcb39b338a241b
Ehtasham29 <96941346+ehtasham29@users.noreply.github.com>
2024-11-29 17:01:06 +0530

    Add files via upload

---
+++ mern-blog-main/client/src/pages/index.js
@@ -0,0 +1,15 @@
+import Home from './Home';
+import About from './About';
+import Dashboard from './Dashboard';
+import Projects from './Projects';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+
+export {
+    Home,
+    About,
+    Dashboard,
+    Projects,
+    SignIn,
+    SignUp
+};
